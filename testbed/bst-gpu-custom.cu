ba0526498442c647c49769f36abc8c79358d94af
Vinodiitmm <karnativinodreddy@gmail.com>
2017-11-12 17:14:48 +0530

    Timer added

--- testbed/bst-gpu-custom.cu
+++ testbed/bst-gpu-custom.cu
@@ -1,7 +1,7 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "timer.h"
 #include "../gpu/bst.cu"
 
 __device__ node* root = NULL;
@@ -87,8 +87,12 @@ int main(int argc, char* argv[]) {
 	//cudaStream_t s1, s2;
 	//cudaStreamCreate(&s1);
 	//cudaStreamCreate(&s2);
+	GPUTimer timer;
+  timer.Start();
 	insert_kernel<<<10,1000>>>();
 	delete_kernel<<<10,1000>>>();
+	timer.Stop();
+	printf("The kernel ran in: %f ms\n", timer.Elapsed());
 	cudaError_t err = cudaGetLastError();  
 	if (err != cudaSuccess)
 		printf("Error: %s\n", cudaGetErrorString(err));
