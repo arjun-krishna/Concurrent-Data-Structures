44d3ccd81dd5a54cc96e24258b3afce9b6b31116
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-12 23:20:57 +0530

    warp based issue

--- testbed/bst-gpu-custom.cu
+++ testbed/bst-gpu-custom.cu
@@ -14,18 +14,21 @@ __global__ void custom_kernel() {
 		root = new_node(0);
 	}
 	__syncthreads();
-	switch (tid) {
-		case 0 : break;
-		case 1 : insert(&root, 1); break;
-		case 2 : insert(&root, 2); break;
-		case 3 : insert(&root, 3); break;
-		case 4 : insert(&root, 4); break;
-		case 5 : insert(&root, 5); break;
-		case 6 : insert(&root, 6); break;
-		case 7 : insert(&root, 7); break;
-		case 8 : insert(&root, 8); break;
-		case 9 : insert(&root, 9); break;
+	if (tid != 0) {
+		insert(root, tid);
 	}
+	// switch (tid) {
+	// 	case 0 : break;
+	// 	case 1 : insert(&root, 1); break;
+	// 	case 2 : insert(&root, 2); break;
+	// 	case 3 : insert(&root, 3); break;
+	// 	case 4 : insert(&root, 4); break;
+	// 	case 5 : insert(&root, 5); break;
+	// 	case 6 : insert(&root, 6); break;
+	// 	case 7 : insert(&root, 7); break;
+	// 	case 8 : insert(&root, 8); break;
+	// 	case 9 : insert(&root, 9); break;
+	// }
 	__syncthreads();
 	if (tid == 0) {
 		printf("In-order\n");
@@ -37,7 +40,7 @@ __global__ void custom_kernel() {
 
 int main(int argc, char* argv[]) {
 	// cudaDeviceSetLimit(cudaLimitMallocHeapSize, 8000000); 
-	custom_kernel<<<1,10>>>();
+	custom_kernel<<<1,4>>>();
 	cudaError_t err = cudaGetLastError();  
 	if (err != cudaSuccess)
 		printf("Error: %s\n", cudaGetErrorString(err));
