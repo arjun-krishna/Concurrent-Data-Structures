9e631fb9d0563c19004c83b1a0739415707f29c3
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-12 22:02:30 +0530

    small fix

--- testbed/bst-gpu-custom.cu
+++ testbed/bst-gpu-custom.cu
@@ -16,15 +16,15 @@ __global__ void custom_kernel() {
 	__syncthreads();
 	switch (tid) {
 		case 0 : break;
-		case 1 : insert(root, 1); break;
-		case 2 : insert(root, 2); break;
-		case 3 : insert(root, 3); break;
-		case 4 : insert(root, 4); break;
-		case 5 : insert(root, 5); break;
-		case 6 : insert(root, 6); break;
-		case 7 : insert(root, 7); break;
-		case 8 : insert(root, 8); break;
-		case 9 : insert(root, 9); break;
+		case 1 : insert(&root, 1); break;
+		case 2 : insert(&root, 2); break;
+		case 3 : insert(&root, 3); break;
+		case 4 : insert(&root, 4); break;
+		case 5 : insert(&root, 5); break;
+		case 6 : insert(&root, 6); break;
+		case 7 : insert(&root, 7); break;
+		case 8 : insert(&root, 8); break;
+		case 9 : insert(&root, 9); break;
 	}
 	__syncthreads();
 	if (tid == 0) {
