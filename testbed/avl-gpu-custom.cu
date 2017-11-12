13454e4895f0f3baefd978f31e36f6649d9e17ca
Vinodiitm <karnativinodreddy@gmail.com>
2017-11-13 04:46:24 +0530

    AVL insert

--- testbed/avl-gpu-custom.cu
+++ testbed/avl-gpu-custom.cu
@@ -4,33 +4,33 @@
 #include "timer.h"
 #include "../gpu/avl.cu"
 
-__device__ node* root;
+//__device__ node* root;
 
 __global__ void custom_kernel() {
 	
-	int tid = threadIdx.x;
+	int tid = blockIdx.x*1000 + threadIdx.x;
 	if (tid == 0) {
 		// insert(root, 0);
-		root = new_node(0, NULL);
+		global_Root = new_node(0, NULL);
 	}
 	__syncthreads();
 	if (tid != 0) {
-		coarse_insert(root, tid);
+		coarse_insert(tid);
 	}
 	__syncthreads();
 	if (tid == 0) {
 		printf("In-order\n");
-		in_order(root);
+		in_order(global_Root);
 		printf("Pre-order\n");
-		pre_order(root);
+		pre_order(global_Root);
 	}
 	__syncthreads();
 }
 
 int main(int argc, char* argv[]) {
 	// cudaDeviceSetLimit(cudaLimitMallocHeapSize, 8000000); 
-	//custom_kernel<<<1,1000>>>();
-	custom_kernel<<<1,10>>>();
+	//custom_kernel<<<1,10>>>();
+	custom_kernel<<<10,1000>>>();
 	cudaError_t err = cudaGetLastError();  
 	if (err != cudaSuccess)
 		printf("Error: %s\n", cudaGetErrorString(err));
