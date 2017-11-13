de2c22217d234164c1bbe627b49c1f681d6f1b7b
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-13 10:44:33 +0530

    Attempt AVL-fine insert

--- gpu/avl-fine.cu
+++ gpu/avl-fine.cu
@@ -7,11 +7,17 @@ typedef struct node {
 	struct node *parent;
 	struct node *left;
 	struct node *right;
+	int height;
 	int sema;
 } node;
 
+__device__ node* global_root = NULL;
+
+__device__ volatile int MASTER_LOCK = 0;
+
 __device__ int lock(node* n) {
-	return !atomicExch(&n->sema, 1);
+	int status = atomicExch(&n->sema, 1);
+	return (!status && !MASTER_LOCK);
 }
 
 __device__ void unlock(node* n) {
@@ -23,6 +29,7 @@ __device__ node* new_node(int val, node* parent) {
 	tmp->data = val;
 	tmp->parent = parent;
 	tmp->left = tmp->right = NULL;
+	tmp->height = 1;
 	tmp->sema = 0;
 	return tmp;
 }
@@ -33,6 +40,105 @@ __device__ node* find(node* root, int key) {
 	if (root->data == key) return root;
 	else if (root->data > key) return find(root->left, key);
 	else return find(root->right, key);
+}
+
+__device__ int height(node *root)
+{
+  if (root == NULL)
+      return 0;
+  return root->height;
+}
+
+__device__ int get_balance(node *root)
+{
+  if (root == NULL)
+      return 0;
+  return height(root->left) - height(root->right);
+}
+
+
+node* left_rotate(node* root, node* parent)
+{
+  node* temp1 = root->right;
+  node* temp2 = temp1->left;
+
+  temp1->left = root;
+  root->parent = temp1;
+  root->right = temp2;
+
+  if (temp2)
+  	temp2->parent = root;
+
+  root->height = max(height(root->left), height(root->right))+1;
+  temp1->height = max(height(temp1->left), height(temp1->right))+1;
+
+  temp1->parent = parent;
+  return temp1;
+}
+
+node* right_rotate(node* root, node* parent)
+{
+  node* temp1 = root->left;
+  node* temp2 = temp1->right;
+
+  temp1->right = root;
+  root->parent = temp1;
+  root->left = temp2;
+
+  if(temp2)
+  	temp2->parent = root;
+
+  root->height = max(height(root->left), height(root->right))+1;
+  temp1->height = max(height(temp1->left), height(temp1->right))+1;
+
+  temp1->parent = parent;
+  return temp1;
+}
+
+
+__device__ void rebalance(node* root, int key) {
+	root->height = max(height(root->left),height(root->right)) + 1;
+	int balance = get_balance(root);
+
+	// Left Left Case
+  if (balance > 1 && key < root->left->data) {
+		if (root->parent)
+			root->parent = right_rotate(root, root->parent);
+		else 
+			global_root = right_rotate(root, global_root);
+	}
+
+	// Right Right Case
+  else if (balance < -1 && key > root->right->data) {
+  	if (root->parent)
+  		root->parent = left_rotate(root, root->parent);
+  	else
+  		global_root = right_rotate(root, global_root);
+  }
+
+	// Left Right Case
+  else if (balance > 1 && key > root->left->data) {
+  	root->left =  left_rotate(root->left, root);
+  	if (root->parent)
+  		root->parent = right_rotate(root, root->parent);
+  	else 
+  		global_root = right_rotate(root, global_root);
+  }
+
+	// Right Left Case
+  else if (balance < -1 && key < root->right->data)
+  {
+		root->right = right_rotate(root->right);
+		if (root->parent)
+			root->right = left_rotate(root, root->parent);
+		else
+			global_root = right_rotate(root, global_root);
+  }
+  else {
+  	if (root->parent)
+  		rebalance(root->parent, key);
+  }
+  return;
 }
 
 
@@ -50,6 +156,9 @@ __device__ void insert(node* root, int key) {
 			if (root->left == NULL) {			// Can be inserted to the immediate left
 				root->left = new_node(key, root);
 				unlock(root);
+				while (!atomicExch(&MASTER_LOCK, 1));
+				rebalance(root, key);
+				atomicExch(&MASTER_LOCK, 0);
 				return;
 			} else {											// Release this Node and proceed
 				unlock(root);
@@ -59,12 +168,17 @@ __device__ void insert(node* root, int key) {
 			if (root->right == NULL) {		// Can be inserted to the immediate right
 				root->right = new_node(key, root);
 				unlock(root);
+				while (!atomicExch(&MASTER_LOCK, 1));
+				rebalance(root, key);
+				atomicExch(&MASTER_LOCK, 0);
 				return;
 			} else {
 				unlock(root);								// Release this Node and proceed
 				insert(root->right, key);
 			}
 		}
+		
+
 	} else {
 		insert(root, key);
 	}
---
+++ testbed/fine-avl-custom.cu
@@ -0,0 +1,37 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "timer.h"
+#include "../gpu/avl.cu"
+
+__global__ void custom_kernel() {
+	
+	int tid = threadIdx.x;
+	if (tid == 0) {
+		// insert(root, 0);
+		global_root = new_node(0, NULL);
+	}
+	__syncthreads();
+	if (tid != 0) {
+		insert(global_root, tid);
+	}
+	__syncthreads();
+	if (tid == 0) {
+		printf("In-order\n");
+		in_order(global_root);
+		printf("Pre-order\n");
+		pre_order(global_root);
+	}
+	__syncthreads();
+}
+
+int main(int argc, char* argv[]) {
+	// cudaDeviceSetLimit(cudaLimitMallocHeapSize, 8000000); 
+	//custom_kernel<<<1,1000>>>();
+	custom_kernel<<<1,3>>>();
+	cudaError_t err = cudaGetLastError();  
+	if (err != cudaSuccess)
+		printf("Error: %s\n", cudaGetErrorString(err));
+	cudaDeviceSynchronize();
+	return 0;
+}
