99ca56722af530f82fed7560d7fa40ae1cfc61e6
Vinodiitm <karnativinodreddy@gmail.com>
2017-11-13 20:26:52 +0530

    bst-cpu-test

--- testbed/bst-cpu.cc
+++ testbed/bst-cpu.cc
@@ -18,8 +18,14 @@ void delete_N(int N) {
 			root = bst_delete(root, i);
 }
 
+void find_N(int N) {
+	int i;
+	for (i=0; i<N; i++)
+		find(root, i);
+}
+
 int main(void) {	
-	CPUTimer time_insert, time_delete;
+	CPUTimer time_insert, time_delete, time_find;
 	time_insert.Start();
 	insert_N(10);
 	time_insert.Stop();
@@ -28,8 +34,13 @@ int main(void) {
 	delete_N(10);
 	time_delete.Stop();
 
+	time_find.Start();
+	find_N(10);
+	time_find.Stop();
+
 	printf("Insert took: %f ms\n", time_insert.Elapsed());
 	printf("Delete took: %f ms\n", time_delete.Elapsed());
+	printf("Find   took: %f ms\n", time_find.Elapsed());
 	
 	in_order(root);
 	printf("\n");
